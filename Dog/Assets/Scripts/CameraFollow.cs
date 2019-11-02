5315c02326ba371a18a7fd81889ec6a36443372a
Samuel Förström <samppa.f@gmail.com>
2019-11-02 13:13:00 +0200

    Add tile editor and some placeholders

--- Dog/Assets/Scripts/CameraFollow.cs
+++ Dog/Assets/Scripts/CameraFollow.cs
@@ -2,17 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class CameraFollow : MonoBehaviour {
+  public GameObject target;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  // Update is called once per frame
+  void Update() {
+    var pos = transform.position;
+    var tarPos = target.transform.position;
+
+    transform.position = new Vector3(tarPos.x, pos.y, pos.z);
+  }
 }
