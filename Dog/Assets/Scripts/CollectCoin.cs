9709299e425b25ce13e6ecef090d36ccc9e20812
Samuel Förström <samppa.f@gmail.com>
2019-11-03 18:40:09 +0200

    asd

--- Dog/Assets/Scripts/CollectCoin.cs
+++ Dog/Assets/Scripts/CollectCoin.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CollectCoin : MonoBehaviour {
+
   public int points = 100;
   void OnTriggerEnter2D(Collider2D col) {
     col.GetComponent<PointControl>().Points += points;
