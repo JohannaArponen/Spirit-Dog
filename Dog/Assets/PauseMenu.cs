2d972356323b4097cb328e5e5e38c89b98c5c08f
Johanna Arponen <johanna.arponen@icloud.com>
2019-11-04 09:46:30 +0200

    Pausen teko kesken

---
+++ Dog/Assets/PauseMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour{
+
+	public static bool GameIsPause = false;
+   
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Escape)){
+			if(GameIsPause){
+				Resume();
+			}else{
+				Pause();
+			}
+		}
+    }
+
+	void Re
+}
